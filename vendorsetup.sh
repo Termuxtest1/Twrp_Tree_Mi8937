a30b4358e3840730e6e45371d9166f038fe5f1ed
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-05-26 18:25:11 +0530

    Update VS from omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Mi8937-user
-add_lunch_combo omni_Mi8937-userdebug
-add_lunch_combo omni_Mi8937-eng
+add_lunch_combo twrp_Mi8937-user
+add_lunch_combo twrp_Mi8937-userdebug
+add_lunch_combo twrp_Mi8937-eng
