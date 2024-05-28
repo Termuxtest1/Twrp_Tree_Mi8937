4d7364f289309b90533ca5109499f98174fe376b
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-05-28 09:37:17 +0530

    Update BoardConfig.mk fix brightness

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,3 +95,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_MAX_BRIGHTNESS :=255
+TW_DEFAULT_BRIGHTNESS := 150
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
