e499c545f46f51ae880108b9f995773bd39728ab
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-05-29 12:27:17 +0530

    Update vendorsetup.sh to Support Common Lunch Choices and Some Common Fox Flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+#Some Flags for OrangeFox
+export FOX_ENABLE_APP_MANAGER=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_USE_DYNAMIC_PARTITIONS=1
+
+#Fix Mi8937 device's Not Mi8937 device.
+export TARGET_DEVICE_ALT="land"
+export TARGET_DEVICE_ALT="prada"
+export TARGET_DEVICE_ALT="riva"
+export TARGET_DEVICE_ALT="rolex"
+export TARGET_DEVICE_ALT="santoni"
+export TARGET_DEVICE_ALT="ugglite"
+export TARGET_DEVICE_ALT="ugg"
+
+#Lunch Choices.
 COMMON_LUNCH_CHOICES := \
     twrp_Mi8937-user \
     twrp_Mi8937-userdebug \
