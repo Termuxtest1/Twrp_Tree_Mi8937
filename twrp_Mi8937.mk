b7199409961fdc07d0960dcb7fc5ba35173ca4f7
Termux <89096430+Termuxtest1@users.noreply.github.com>
2024-06-29 09:49:52 +0530

    Added kernel version

--- twrp_Mi8937.mk
+++ twrp_Mi8937.mk
@@ -5,6 +5,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Kernel
+TARGET_KERNEL_VERSION := 4.19
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
